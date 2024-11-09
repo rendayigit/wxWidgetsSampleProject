5dd7315e5fd440df02880ab0921e7247e168471f
rendayigit <renda.yigit@gmail.com>
2024-11-09 12:09:55 +0300

    remove wxwidgets source code

--- src/main.cpp
+++ src/main.cpp
@@ -19,33 +19,37 @@ private:
 
 enum { ID_HELLO = 1 };
 
-bool MyApp::OnInit() {
-  MyFrame frame;
-  frame.Show(true);
-  return true;
+bool MyApp::OnInit()
+{
+    auto *frame = new MyFrame();
+    frame->Show(true);
+    return true;
 }
 
-MyFrame::MyFrame() : wxFrame(nullptr, wxID_ANY, "Hello World") {
-  wxMenu menuFile;
-  menuFile.Append(ID_HELLO, "&Hello...\tCtrl-H", "Help string shown in status bar for this menu item");
-  menuFile.AppendSeparator();
-  menuFile.Append(wxID_EXIT);
-
-  wxMenu menuHelp;
-  menuHelp.Append(wxID_ABOUT);
-
-  wxMenuBar menuBar;
-  menuBar.Append(&menuFile, "&File");
-  menuBar.Append(&menuHelp, "&Help");
-
-  SetMenuBar(&menuBar);
-
-  CreateStatusBar();
-  SetStatusText("Welcome to wxWidgets!");
-
-  Bind(wxEVT_MENU, &MyFrame::onHello, this, ID_HELLO);
-  Bind(wxEVT_MENU, &MyFrame::onAbout, this, wxID_ABOUT);
-  Bind(wxEVT_MENU, &MyFrame::onExit, this, wxID_EXIT);
+MyFrame::MyFrame()
+    : wxFrame(nullptr, wxID_ANY, "Hello World")
+{
+    auto *menuFile = new wxMenu;
+    menuFile->Append(ID_HELLO, "&Hello...\tCtrl-H",
+                     "Help string shown in status bar for this menu item");
+    menuFile->AppendSeparator();
+    menuFile->Append(wxID_EXIT);
+ 
+    auto *menuHelp = new wxMenu;
+    menuHelp->Append(wxID_ABOUT);
+ 
+    auto *menuBar = new wxMenuBar;
+    menuBar->Append(menuFile, "&File");
+    menuBar->Append(menuHelp, "&Help");
+ 
+    SetMenuBar( menuBar );
+ 
+    CreateStatusBar();
+    SetStatusText("Welcome to wxWidgets!");
+ 
+    Bind(wxEVT_MENU, &MyFrame::onHello, this, ID_HELLO);
+    Bind(wxEVT_MENU, &MyFrame::onAbout, this, wxID_ABOUT);
+    Bind(wxEVT_MENU, &MyFrame::onExit, this, wxID_EXIT);
 }
 
 void MyFrame::onExit(wxCommandEvent & /*event*/) { Close(true); }
